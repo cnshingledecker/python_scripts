47a38b97d8b607413eafc610792eb813eee7da53
Christopher N. Shingledecker <astrochemist@gmx.com>
2015-07-17 12:05:09 -0400

    changed divide

--- divide_rate.py
+++ divide_rate.py
@@ -1,19 +1,51 @@
 # -*- coding: utf-8 -*-
 """
-This script scrubs a file of reactions containing certain species
+This script takes an input file of reactions and divides the alpha value
+by some number specified by the user
 
-@author: cns
+@author: Christopher N. Shingledecker
+@date:   2015-07-16
 """
 
-import reader,sys
+
+filename = 'hh-h2hp-wrong-rates.in'
+divide_by = 2 
+form = '.3e'
+
+def div_rate(filename,divNum):
+    infile = filename
+    inf = open(infile,'r')
+    outfile = 'corrected_' + filename
+    outf = open(outfile,'w')
+#    outf.write(inf.readline()) #uncomment this line if there is a header line in the input file
+    lines = inf.readlines()
+    for line in lines:
+        alpha = float(line[53:63])/divNum
+        print line[53:63], "divided by", str(divNum), "equals %.3e" % alpha
+        newline = line[:53] + "{:.3e}".format(alpha) + '  ' + line[64:]
+        outf.write(newline)
 
 
+    inf.close()
+    outf.close() 
+    bigE(outfile)
+    
+def bigE(filename):
+    infile = filename
+    inf = open(infile,'r')
+    outfile = 'bigE_' + filename
+    outf = open(outfile,'w')
 
-new_reactions = sys.argv[1] 
-startnum = int(sys.argv[2])
+    lines = inf.readlines()
+    for line in lines:
+        newline = line[:58] + 'E' + line[59:]
+        outf.write(newline)
 
-print 'No-arrowing reactions:'
-filename = new_reactions
-reader.renumber(filename,startnum)
-#reader.noarrow(filename)
+    inf.close()
+    outf.close() 
+
+
+print 'Dividing rates:'
+div_rate(filename,divide_by)
+
 
