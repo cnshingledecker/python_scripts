8c8f214403628aad06a7152be177691a168a0a1e
Christopher N. Shingledecker <astrochemist@gmx.com>
2015-08-05 12:07:15 -0400

    changed from linear to log interval spacing

--- OPR_increment.py
+++ OPR_increment.py
@@ -5,27 +5,30 @@ Created on Wed Jul 29 13:52:17 2015
 @author: cns
 """
 
-import random, os,subprocess
+import math,random,os,subprocess
 
 contFile = 'nls_control.d'
 compFile = 'compile.sh'
 exFile = 'nls.bash'
-Nruns = 500 
+Nruns = 500
 nH2 = 0.499975 
 OPR_min = 0.001 
 OPR_max = 3 
-OPR_inc = (OPR_max - OPR_min)/Nruns
 
 
 os.system('rm -rf OPR_run*')
+os.system('rm -rf temp')
 os.system('rm OPR.out')
 
 results = 'OPR.out'
 
+random.seed(716381)
+
 temp_OPR = OPR_min
-for i in range(0,Nruns):
+for j in range(0,Nruns):
     #Mkdir with os package
-    temp_path = 'OPR_run_'+str(i)
+    temp_path = 'temp'
+    os.system('rm -rf temp')
     os.mkdir(temp_path)
     
     #Copy files into new directory
@@ -33,7 +36,8 @@ for i in range(0,Nruns):
     os.system(cmd)
         
     #Calculate new parameter values
-    temp_OPR = temp_OPR + OPR_inc
+    r1 = random.random()
+    temp_OPR = 10**(r1*math.log10(OPR_min) + (1-r1)*math.log10(OPR_max)) 
     pH2 = nH2/(1+temp_OPR)
     oH2 = temp_OPR*pH2
     temp_sum = oH2 + pH2
@@ -80,3 +84,12 @@ for i in range(0,Nruns):
 
     #Change back into home directory
     os.chdir('..')
+    newdir = 'OPR_run_'+str(j)
+    os.mkdir(newdir)
+    cmd = 'cp ./temp/output_1D* ./' + newdir + '/'
+    os.system(cmd)
+    cmd =  'cp ./temp/rates1D.* ./' + newdir + '/'  
+    os.system(cmd)
+    cmd = 'rm -rf temp/'
+    os.system(cmd)
+ 
